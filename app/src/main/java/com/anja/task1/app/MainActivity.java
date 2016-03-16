ff79d3dae7cfd865c9758b83eff23036928b3f3d
severianremi <severianremi@gmail.com>
2016-03-16 21:20:45 +0200

    Add toast for controls

--- app/src/main/java/com/anja/task1/app/MainActivity.java
+++ app/src/main/java/com/anja/task1/app/MainActivity.java
@@ -6,21 +6,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        setOnClickListenerForChildren((ViewGroup) findViewById(R.id.mainLayout));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
         return true;
@@ -36,6 +42,25 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        String msg = v.getClass().getSimpleName() + " id: " + v.getResources().getResourceEntryName(v.getId());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void setOnClickListenerForChildren(ViewGroup parent) {
+        for (int i = parent.getChildCount() - 1; i >= 0; i--) {
+            final View child = parent.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                setOnClickListenerForChildren((ViewGroup) child);
+            } else {
+                if (child != null && child instanceof TextView) {
+                    child.setOnClickListener(this);
+                }
+            }
         }
     }
 }
