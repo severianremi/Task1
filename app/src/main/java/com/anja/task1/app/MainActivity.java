83f9e71587d9a60d9308a8a5a2bd200316bd5bd1
severianremi <severianremi@gmail.com>
2016-04-10 15:02:50 +0300

    Add navigationView

--- app/src/main/java/com/anja/task1/app/MainActivity.java
+++ app/src/main/java/com/anja/task1/app/MainActivity.java
@@ -1,66 +1,111 @@
 package com.anja.task1.app;
 
-
-import android.support.v7.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.design.internal.NavigationMenu;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.BindString;
+import butterknife.ButterKnife;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.nav_footer)
+    TextView navFooterView;
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+    @BindString(R.string.nav_footer_text)
+    String navFooterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-        setOnClickListenerForChildren((ViewGroup) findViewById(R.id.main_layout));
 
-        RecyclerView recyclerViewImages = (RecyclerView) findViewById(R.id.images_layout_id);
-        LinearLayoutManager lm
-                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerViewImages.setLayoutManager(lm);
-        TaskImageRecycleViewAdapter adapter = new TaskImageRecycleViewAdapter();
-        recyclerViewImages.setAdapter(adapter);
+        ButterKnife.bind(this);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navFooterView.setText(Html.fromHtml(navFooterText));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-            default:
-                return super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public void onClick(View v) {
-        String msg = v.getClass().getSimpleName() + " id: " + v.getResources().getResourceEntryName(v.getId());
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
-    public void setOnClickListenerForChildren(ViewGroup parent) {
-        for (int i = 0; i <= parent.getChildCount() - 1; i++) {
-            final View child = parent.getChildAt(i);
-            if (child instanceof ViewGroup) {
-                setOnClickListenerForChildren((ViewGroup) child);
-            } else {
-                if (child != null && child instanceof TextView) {
-                    child.setOnClickListener(this);
-                }
-            }
-        }
-    }
+        FragmentTransaction ftrans = getFragmentManager().beginTransaction();
+//
+//        if (id == R.id.nav_camara) {
+//            ftrans.replace(R.id.container, fimport);
+//        } else if (id == R.id.nav_gallery) {
+//            ftrans.replace(R.id.container, fgallery);
+//
+//        } else if (id == R.id.nav_slideshow) {
+//            ftrans.replace(R.id.container, fshow);
+//
+//        } else if (id == R.id.nav_manage) {
+//            ftrans.replace(R.id.container, ftools);
+//
+//        } else if (id == R.id.nav_share) {
+//            ftrans.replace(R.id.container, fshare);
+//
+//        } else if (id == R.id.nav_send) {
+//            ftrans.replace(R.id.container, fsend);
+//
+//        } ftrans.commit();
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
